dfc4bdf11f8886c91a1cc5ea213691256d529236
johnklee <puremonkey2001@yahoo.com.tw>
2025-03-02 09:08:52 +0000

    Implements #56

---
+++ src/cockatoo_ai/utils/model_a/__init__.py
@@ -0,0 +1,44 @@
+"""Module to hold base class or protocols used in model A."""
+import enum
+from typing import Any
+from cockatoo_ai.utils import wrapper
+from cockatoo_ai.utils.model_a import speech_recognition_wrapper
+
+
+LangEnum = wrapper.LangEnum
+
+
+class ModelType(enum.StrEnum):
+  """Model A types."""
+  SR_OPEN_AI_WHISPER = 'sr_open_ai_whisper'
+  SR_GCP = 'sr_gcp'
+  GCP_SPEECH_2_TEXT = 'gcp_speech_2_text'
+
+
+def get(
+    model_type: ModelType | str,
+    settings: dict[str, Any] | None = None) -> wrapper.ModelA:
+  """Gets model A.
+
+  Args:
+    model_type: Model type.
+    settings: Model settings.
+
+  Returns:
+    Model A wrapper implementation.
+  """
+  match model_type:
+    case ModelType.SR_OPEN_AI_WHISPER:
+      if not settings:
+        settings = {'lang': LangEnum.en}
+      return speech_recognition_wrapper.SRWhisperWrapper(**settings)
+    case ModelType.SR_GCP:
+      if not settings:
+        settings = {'lang': LangEnum.en}
+      return speech_recognition_wrapper.SRGoogleWrapper(**settings)
+    case ModelType.GCP_SPEECH_2_TEXT:
+      if not settings:
+        settings = {'lang': LangEnum.en}
+      return speech_recognition_wrapper.GCPSpeech2TextWrapper(**settings)
+    case _:
+      raise ValueError('Invalid model type="{model_type}"!')
